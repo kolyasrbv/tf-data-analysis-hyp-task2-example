136b50d0653fbd677dea7d315b44df4f23de17aa
Nikolay Serebryakov <116488084+kolyasrbv@users.noreply.github.com>
2023-04-12 23:39:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,19 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    simulations = 1000
+    n_s = 1000
+    res = []
+    # Running A/A Test Simulations
+    x = pd.DataFrame(x)
+    y = pd.DataFrame(y)
+    for i in tqdm(range(simulations)):
+        res.append(stats.ttest_ind(x.sample(n_s, replace = False), \
+                                   y.sample(n_s, replace = False), \
+                                   equal_var = False)[1]) # saving p-value
+# Checking that the number of false positive cases does not exceed alpha
+    m = sum(np.array(res) < 0.02) / simulations
+    if m < 0.02:
+        return False
+    return True
+solution(x, y)
