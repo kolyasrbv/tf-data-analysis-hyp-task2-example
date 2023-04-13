0a81cb94b4d927cca384f0fe46bcae9d41d26f30
Nikolay Serebryakov <116488084+kolyasrbv@users.noreply.github.com>
2023-04-13 14:56:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,23 +1,16 @@
 import pandas as pd
 import numpy as np
-
+import scipy.stats as st
 
 chat_id = 433193277 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    simulations = 1000
-    n_s = 1000
-    res = []
-    # Running A/A Test Simulations
-    x = pd.DataFrame(x)
-    y = pd.DataFrame(y)
-    for i in tqdm(range(simulations)):
-        res.append(stats.ttest_ind(x.sample(n_s, replace = False), \
-                                   y.sample(n_s, replace = False), \
-                                   equal_var = False)[1]) # saving p-value
-# Checking that the number of false positive cases does not exceed alpha
-    m = sum(np.array(res) < 0.02) / simulations
-    if m < 0.02:
-        return False
-    return True
-solution(x, y)
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    alpha = 0.02
+    a = st.anderson_ksamp([x, y]).significance_level
+    
+    return a < alpha 
+
+433193277
